ca158ce26f56e7d8353953a2bf16e5d04c762dfa
axothy <globaltouareg@gmail.com>
2024-10-02 15:55:27 +0300

    Изначальный коммит

---
+++ src/main/java/ru/axothy/airline/AirlineCompanyApplication.java
@@ -0,0 +1,13 @@
+package ru.axothy.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineCompanyApplication.class, args);
+    }
+
+}
